1c00937425df6e477d98a22ac93286119fa98857
seungyeon <hanstom0608@naver.com>
2025-07-24 09:44:38 +0900

    feat: router설정, 폴더구조 작성

---
+++ frontend/apps/user/src/components/layout/MobileLayout.tsx
@@ -0,0 +1,9 @@
+import { Outlet } from 'react-router-dom';
+
+export default function MobileLayout() {
+  return (
+    <div className="mx-auto min-h-screen w-full max-w-md bg-white">
+      <Outlet />
+    </div>
+  );
+}
