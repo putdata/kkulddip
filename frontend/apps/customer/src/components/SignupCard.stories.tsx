1aca727b70a37912e17dc23a42264ca96e940a05
홍혜린 <shift6210@gmail.com>
2025-07-30 11:25:38 +0900

    feat: 회원가입 카드 임시 제작

---
+++ frontend/apps/customer/src/components/SignupCard.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import SignupCard from './SignupCard';
+import { MemoryRouter } from 'react-router-dom';
+
+const meta: Meta<typeof SignupCard> = {
+  title: 'Components/SignupCard',
+  component: SignupCard,
+  decorators: [
+    Story => (
+      <MemoryRouter>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof SignupCard>;
+
+export const Default: Story = {
+  args: {},
+};
