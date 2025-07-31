38638d8d0d09db47c1cf5b249112462249e86dd2
홍혜린 <shift6210@gmail.com>
2025-07-31 13:54:38 +0900

    feat: MobileLayout 작성

---
+++ frontend/apps/customer/src/components/layout/MobileLayoutWithNavbar.tsx
@@ -0,0 +1,11 @@
+import BottomNavbar from '@/components/common/BottomNavbar/BottomNavbar';
+import { Outlet } from 'react-router-dom';
+
+export default function MobileLayoutWithNavBar() {
+  return (
+    <div className="mx-auto min-h-screen w-full max-w-md bg-white">
+      <Outlet />
+      <BottomNavbar />
+    </div>
+  );
+}
