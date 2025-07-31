adb57e352085d6d2917ea35ec998162e05c8c14b
홍혜린 <shift6210@gmail.com>
2025-07-31 15:49:15 +0900

    design: MobileLayout 하단 Navbar 삭제

--- frontend/apps/customer/src/components/layout/MobileLayout.tsx
+++ frontend/apps/customer/src/components/layout/MobileLayout.tsx
@@ -1,11 +1,9 @@
-import BottomNavbar from '@/components/common/BottomNavbar/BottomNavbar';
 import { Outlet } from 'react-router-dom';
 
 export default function MobileLayout() {
   return (
     <div className="mx-auto min-h-screen w-full max-w-md bg-white">
       <Outlet />
-      <BottomNavbar />
     </div>
   );
 }
