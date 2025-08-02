f191df7c30d3d35716ea82f7c9467a4694614ae5
meowdevhyerin <meowdevhyerin@gmail.com>
2025-08-02 20:24:39 +0900

    refactor: userStore 생성

---
+++ frontend/packages/common/src/stores/userStore.ts
@@ -0,0 +1,28 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+interface User {
+  email: string;
+  name: string;
+  role: string;
+  profileImageUrl: string;
+}
+
+interface UserState {
+  user: User | null;
+  setUser: (user: User) => void;
+  clearUser: () => void;
+}
+
+export const useUserStore = create<UserState>()(
+  persist(
+    set => ({
+      user: null,
+      setUser: user => set({ user }),
+      clearUser: () => set({ user: null }),
+    }),
+    {
+      name: 'user-storage',
+    },
+  ),
+);
