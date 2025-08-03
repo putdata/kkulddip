5764a6dfae52b792107168cc8f2bbb30a28656a4
young-droid <young.yj.cho@gmail.com>
2025-08-04 01:53:00 +0900

    refactor: 리뷰 작성 페이지 데이터를 constants 폴더로 분리

--- frontend/apps/customer/src/pages/review/ReviewCreate.tsx
+++ frontend/apps/customer/src/pages/review/ReviewCreate.tsx
@@ -2,28 +2,25 @@ import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
 import { Textarea } from '@/components/ui/textarea';
 import { Camera, Star } from 'lucide-react';
-import React from 'react';
+
+import { reviewCreateMessages } from '@/constants/messages';
+import { reviewCreateMockData } from '@/constants/mockData';
 
 // export interface Props {
+//   storeId: number;
 //   storeName: string;
 //   img: string;
-//   img-alt: string;
+//   imgAlt: string;
 // }
-
-const props = {
-  // 임시 데이터
-  storeName: '도미노피자 역삼점',
-  img: 'https://images.unsplash.com/photo-1550321989-65d089904d5c?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  // 현 페이지에서 사용할 문구
-  question1: '음식은 어떠셨나요?',
-  description1: '자세한 띱박스 리뷰를 작성해주세요.',
-  placeholder1: '여러분의 따뜻한 리뷰는\n가게 사장님들에게 큰 도움이 됩니다.',
-  addPicture: '사진 추가',
-  pictureCount: '( 0 / 3 )',
-};
 
 // const ReviewCreate = ({ storeName }: Props) => {
 const ReviewCreate = () => {
+  // ui 문구
+  const messages = reviewCreateMessages;
+
+  // 예시 데이터
+  const props = reviewCreateMockData;
+
   return (
     // 임시 - div 확인용 스타일
     <>
@@ -45,7 +42,7 @@ const ReviewCreate = () => {
             </div>
             <div className="inline-flex flex-[0_0_auto] items-end">
               <div className="text-sm font-bold text-gray-900 [font-family:'Segoe_UI']">
-                {props.question1}
+                {messages.question1}
               </div>
             </div>
             <div className="flex items-center">
@@ -59,7 +56,7 @@ const ReviewCreate = () => {
           </div>
 
           <div className="flex h-20 w-20 items-center justify-center overflow-hidden bg-amber-100 p-[0.8px]">
-            <img alt="Pizza" src={props.img} />
+            <img alt={props.imgAlt} src={props.img} />
           </div>
         </div>
 
@@ -67,12 +64,12 @@ const ReviewCreate = () => {
 
         <div className="flex flex-col items-start gap-2 px-5 py-2.5">
           <div className="w-fit whitespace-nowrap text-sm font-bold text-gray-900 [font-family:'Segoe_UI']">
-            {props.description1}
+            {messages.description1}
           </div>
           <div className="flex w-full items-start gap-1.5 self-stretch">
             <Textarea
               className="h-50 resize-none bg-white p-2.5"
-              placeholder={props.placeholder1}
+              placeholder={messages.placeholder1}
             />
           </div>
         </div>
@@ -81,17 +78,17 @@ const ReviewCreate = () => {
           <Button className="pointer-events-none flex h-auto w-full flex-1 grow items-center justify-center border border-dashed border-amber-600 bg-transparent text-amber-600">
             <Camera className="h-6 w-6" />
             <span className="text-lg font-bold [font-family:'segoe_UI',Helvetica]">
-              {props.addPicture}
+              {messages.addPicture}
             </span>
             <span className="text-lg font-bold [font-family:'segoe_UI',Helvetica]">
-              {props.pictureCount}
+              {messages.pictureCount}
             </span>
           </Button>
         </div>
 
         <div className="flex justify-center">
           <Button className="w-50 pointer-events-none flex bg-amber-500 text-center font-bold text-white [font-family:Helvetica]">
-            등록하기
+            {messages.submitButton}
           </Button>
         </div>
       </div>
