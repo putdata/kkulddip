1e8d5de8ebc121959a086bc1ff62f8a617ec8320
young-droid <young.yj.cho@gmail.com>
2025-08-04 01:53:00 +0900

    refactor: 리뷰 작성 페이지 데이터를 constants 폴더로 분리

---
+++ frontend/apps/customer/src/constants/messages.ts
@@ -0,0 +1,11 @@
+/**
+ * 리뷰 작성 페이지 UI 문구
+ */
+export const reviewCreateMessages = {
+  question1: '음식은 어떠셨나요?',
+  description1: '자세한 띱박스 리뷰를 작성해주세요.',
+  placeholder1: '여러분의 따뜻한 리뷰는\n가게 사장님들에게 큰 도움이 됩니다.',
+  addPicture: '사진 추가',
+  pictureCount: '( 0 / 3 )',
+  submitButton: '등록하기',
+};
