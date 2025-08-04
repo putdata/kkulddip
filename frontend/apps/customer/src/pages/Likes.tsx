4c279edcd6542ff5cb2597fd938e5075c408eafc
홍혜린 <shift6210@gmail.com>
2025-08-04 13:13:06 +0900

    design: 스크롤 가능하게 dummyData수정

--- frontend/apps/customer/src/pages/Likes.tsx
+++ frontend/apps/customer/src/pages/Likes.tsx
@@ -58,6 +58,63 @@ const dummyData = [
     distance: 0.5,
     remainingQuantity: 8,
   },
+  {
+    storeInfo: {
+      storeName: '버거킹 강남역점',
+      description: '와퍼세트, 너겟킹',
+      ratingAverage: 4.1,
+    },
+    img: {
+      src: 'https://mob-prd.burgerking.co.kr/images/menu/web/main/2025/01/06/c8bd99f7-0ce8-457c-8824-4360d6b08d82.png',
+      alt: '버거 이미지',
+    },
+    price: {
+      original: 15000,
+      discount: 9900,
+    },
+    timeLeftHour: 1,
+    discountRate: 34,
+    distance: 0.5,
+    remainingQuantity: 8,
+  },
+  {
+    storeInfo: {
+      storeName: '버거킹 강남역점',
+      description: '와퍼세트, 너겟킹',
+      ratingAverage: 4.1,
+    },
+    img: {
+      src: 'https://mob-prd.burgerking.co.kr/images/menu/web/main/2025/01/06/c8bd99f7-0ce8-457c-8824-4360d6b08d82.png',
+      alt: '버거 이미지',
+    },
+    price: {
+      original: 15000,
+      discount: 9900,
+    },
+    timeLeftHour: 1,
+    discountRate: 34,
+    distance: 0.5,
+    remainingQuantity: 8,
+  },
+  {
+    storeInfo: {
+      storeName: '버거킹 강남역점',
+      description: '와퍼세트, 너겟킹',
+      ratingAverage: 4.1,
+    },
+    img: {
+      src: 'https://mob-prd.burgerking.co.kr/images/menu/web/main/2025/01/06/c8bd99f7-0ce8-457c-8824-4360d6b08d82.png',
+      alt: '버거 이미지',
+    },
+    price: {
+      original: 15000,
+      discount: 9900,
+    },
+    timeLeftHour: 1,
+    discountRate: 34,
+    distance: 0.5,
+    remainingQuantity: 8,
+  },
 ];
 
 const Likes = () => {
