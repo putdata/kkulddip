f67f632dce9623972061ea4426f7890b56c8f537
홍혜린 <shift6210@gmail.com>
2025-08-05 17:24:01 +0900

    feat: 더미데이터 상수 분리 및 페이지 적용

--- frontend/apps/customer/src/pages/Orders.tsx
+++ frontend/apps/customer/src/pages/Orders.tsx
@@ -1,7 +1,14 @@
+import OrderCard from '@/components/pages/orders/OrderCard/OrderCard';
+import { orderDummyData } from '@/dummies/orderDummy';
+
 const Orders = () => {
   return (
-    <div className="flex flex-col items-center justify-between bg-orange-50 p-0">
-      <div>주문 내역</div>
+    <div className="px-4 py-4 pb-16 pt-16">
+      <div className="space-y-4">
+        {orderDummyData.map(order => (
+          <OrderCard key={order.orderId} item={order} />
+        ))}
+      </div>
     </div>
   );
 };
