e17c9ba2cb88b1e8672537c502fa4248dadf22f1
young.yj.cho <young.yj.cho@gmail.com>
2025-08-06 13:34:35 +0900

    remove: review 전체보기 파일 충돌 우려되어 삭제

--- frontend/apps/customer/src/pages/review/Reviews.tsx
+++
@@ -1,9 +0,0 @@
-const Reviews = () => {
-  return (
-    <div className="flex flex-col items-center justify-between bg-orange-50 p-0">
-      <div>전체 리뷰 화면</div>
-    </div>
-  );
-};
-
-export default Reviews;
