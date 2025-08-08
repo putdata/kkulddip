8bf7ade8f4f9e5e10346b12fca485d845a8d8a41
young.yj.cho <young.yj.cho@gmail.com>
2025-08-08 16:00:33 +0900

    delete: 가게 대표 메뉴 컴포넌트 삭제

--- frontend/apps/customer/src/components/pages/storeDetail/StoreDetailContainer/StoreDetailContainer.tsx
+++ frontend/apps/customer/src/components/pages/storeDetail/StoreDetailContainer/StoreDetailContainer.tsx
@@ -1,7 +1,6 @@
 import type { StoreDetailDto } from '@/dummies/storeDetailDummy';
 import { DdipboxContainer } from './DdipboxContainer/DdipboxContainer';
 import { StoreDescription } from './StoreDescription';
-import { StoreMainMenu } from './StoreMainMenu';
 
 interface StoreDetailHeaderProps {
   store: StoreDetailDto;
@@ -10,7 +9,6 @@ interface StoreDetailHeaderProps {
 export const StoreDetailContainer = ({ store }: StoreDetailHeaderProps) => {
   return (
     <div className="min-w-sm flex w-full flex-col items-center gap-2">
-      <StoreMainMenu />
       <DdipboxContainer store={store} />
       <StoreDescription store={store} />
     </div>
