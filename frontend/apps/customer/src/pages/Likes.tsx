85b4b9f7f1a5bd86ea0bf88e197a7172692340ff
홍혜린 <meowdevhyerin@gmail.com>
2025-08-09 19:41:28 +0900

    feat: 찜 관련 API 호출 서비스 구현

--- frontend/apps/customer/src/pages/Likes.tsx
+++ frontend/apps/customer/src/pages/Likes.tsx
@@ -1,13 +1,38 @@
 import LikeFoodCard from '@/components/pages/likes/LikeFoodCard/LikeFoodCard';
-import { dummyLikeFoodData } from '@/dummies/dummyFoodData';
+import { useLikes } from '@/hooks/useLikes';
+// import { dummyLikeFoodData } from '@/dummies/dummyFoodData';
 
 const Likes = () => {
+  const { stores } = useLikes(5);
+
+  const transformedStores = stores.map(store => ({
+    img: {
+      src: store.storeProfileImage,
+      alt: store.storeName,
+    },
+    storeInfo: {
+      storeName: store.storeName,
+      ratingAverage: store.reviewRating,
+    },
+    distance: store.distanceFromCustomer,
+    price: {
+      discount: 0,
+    },
+    discountRate: 0,
+  }));
+
   return (
     <div>
       <div className="space-y-2 px-2 pb-16 pt-16">
-        {dummyLikeFoodData.map((item, idx) => (
-          <LikeFoodCard key={idx} item={item} />
-        ))}
+        {transformedStores.map((item, idx) => {
+          return (
+            <LikeFoodCard
+              key={stores[idx]?.storeId}
+              item={item}
+              onClick={() => console.log('매장 클릭')}
+            />
+          );
+        })}
       </div>
     </div>
   );
