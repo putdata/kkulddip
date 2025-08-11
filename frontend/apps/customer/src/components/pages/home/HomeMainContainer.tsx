b14967f6a9e9d18617e6f88e78530c3e21f0af53
young.yj.cho <young.yj.cho@gmail.com>
2025-08-11 12:54:39 +0900

    feat: 홈 페이지 뷰

---
+++ frontend/apps/customer/src/components/pages/home/HomeMainContainer.tsx
@@ -0,0 +1,22 @@
+import FoodCard from '@/components/common/FoodCard/FoodCard';
+import type { Store } from '@/constants/homeMockData';
+
+interface HomeMainProps {
+  stores: Store[];
+}
+
+const HomeMainContainer = ({ stores }: HomeMainProps) => {
+  if (stores.length === 0) {
+    return <div className="p-5">표시할 매장이 없습니다.</div>;
+  }
+
+  return (
+    <div className="flex flex-col gap-3 p-5">
+      {stores.map(store => (
+        <FoodCard store={store} />
+      ))}
+    </div>
+  );
+};
+
+export default HomeMainContainer;
