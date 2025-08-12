f3be5023206042202dbd08682d0427387d7ac130
홍혜린 <meowdevhyerin@gmail.com>
2025-08-12 23:31:56 +0900

    refactor: ordersService에서 하드코딩된 URL을 API_PATH 상수로 대체

--- frontend/apps/customer/src/constants/api-path.ts
+++ frontend/apps/customer/src/constants/api-path.ts
@@ -1,3 +1,4 @@
 export const API_PATH = {
   AUTH_EXCHANGE_TOKEN: '/v1/auth/customer/token',
+  ORDERS_MY_HISTORY: '/v1/orders/my-history',
 };
--- frontend/apps/customer/src/services/ordersService.ts
+++ frontend/apps/customer/src/services/ordersService.ts
@@ -1,7 +1,9 @@
 import { apiClient } from 'common';
 import { type OrderFoodItem } from '@/types/orderFood';
+import { API_PATH } from '@/constants/api-path';
+
 
 export const getOrders = async (): Promise<OrderFoodItem[]> => {
-  const response = await apiClient.get<OrderFoodItem[]>('v1/orders/my-history');
+  const response = await apiClient.get<OrderFoodItem[]>(API_PATH.ORDERS_MY_HISTORY);
   return response;
 };
