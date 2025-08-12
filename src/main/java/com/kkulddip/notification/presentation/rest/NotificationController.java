c660ce9d724686fd4acfbb9c1dad05732f4c0506
오익준 <put_data@naver.com>
2025-08-12 15:47:02 +0900

    fix: fcm-tokens / notifications 도메인 api 경로 수정

--- src/main/java/com/kkulddip/notification/presentation/rest/NotificationController.java
+++ src/main/java/com/kkulddip/notification/presentation/rest/NotificationController.java
@@ -27,7 +27,7 @@ import java.util.List;
  */
 @Slf4j
 @RestController
-@RequestMapping("/api/notifications")
+@RequestMapping("/v1/notifications")
 @RequiredArgsConstructor
 public class NotificationController implements NotificationApi {
 
