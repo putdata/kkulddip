1e359319d5f95f859c84400c47a4f8dccc4e509d
홍혜린 <shift6210@gmail.com>
2025-08-12 13:11:05 +0900

    feat: 컴포넌트 수정

--- frontend/apps/customer/src/pages/Orders.tsx
+++ frontend/apps/customer/src/pages/Orders.tsx
@@ -1,11 +1,13 @@
 import OrderCard from '@/components/pages/orders/OrderCard/OrderCard';
-import { orderDummyData } from '@/dummies/orderDummy';
+import { useOrders } from '@/hooks/useOrders';
 
 const Orders = () => {
+  const { data: orders } = useOrders();
+
   return (
     <div className="px-4 py-4 pb-16 pt-16">
       <div className="space-y-4">
-        {orderDummyData.map(order => (
+        {orders?.map(order => (
           <OrderCard key={order.orderId} item={order} />
         ))}
       </div>
