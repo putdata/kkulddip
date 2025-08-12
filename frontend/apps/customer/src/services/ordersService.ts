ed93de19472161ad1a6159af6600e26e42c43700
홍혜린 <meowdevhyerin@gmail.com>
2025-08-12 23:43:33 +0900

    refactor: format 수정

--- frontend/apps/customer/src/services/ordersService.ts
+++ frontend/apps/customer/src/services/ordersService.ts
@@ -2,8 +2,9 @@ import { apiClient } from 'common';
 import { type OrderFoodItem } from '@/types/orderFood';
 import { API_PATH } from '@/constants/api-path';
 
-
 export const getOrders = async (): Promise<OrderFoodItem[]> => {
-  const response = await apiClient.get<OrderFoodItem[]>(API_PATH.ORDERS_MY_HISTORY);
+  const response = await apiClient.get<OrderFoodItem[]>(
+    API_PATH.ORDERS_MY_HISTORY,
+  );
   return response;
 };
