c2e2de0556e042b97fe19bcd2afe4dfc5532d90b
young.yj.cho <young.yj.cho@gmail.com>
2025-08-13 10:51:24 +0900

    refactor: 검색바 nav로 이동

--- frontend/apps/customer/src/components/common/NavigationBar/NavigationBar.tsx
+++ frontend/apps/customer/src/components/common/NavigationBar/NavigationBar.tsx
@@ -1,25 +1,29 @@
+import SearchBar from '@/components/pages/home/SearchBar';
 import { MapPin, Bell, Menu } from 'lucide-react';
 
 const NavigationBar = () => {
   return (
-    <nav className="flex items-center justify-between bg-white px-4 py-3 shadow-md">
-      <div className="flex items-center space-x-2">
-        <img src="/logo.png" alt="logo" className="h-6 w-6" />
-        <span className="text-lg font-semibold text-amber-600">꿀띱</span>
-      </div>
+    <nav className="flex flex-col gap-2 bg-white px-4 py-3 shadow-md">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center space-x-2">
+          <img src="/logo.png" alt="logo" className="h-6 w-6" />
+          <span className="text-lg font-semibold text-amber-600">꿀띱</span>
+        </div>
 
-      <div className="flex items-center space-x-4">
-        <button className="flex items-center rounded-3xl bg-gray-100 p-1.5 pr-2.5 text-sm text-gray-700">
-          <MapPin className="mr-1 h-4 w-4" />
-          강남구
-        </button>
-        <button className="flex items-center text-gray-700">
-          <Bell className="mr-1 h-4 w-4" />
-        </button>
-        <button className="text-sm text-gray-700">
-          <Menu className="mr-1 h-4 w-4" />
-        </button>
+        <div className="flex items-center space-x-4">
+          <button className="flex items-center rounded-3xl bg-gray-100 p-1.5 pr-2.5 text-sm text-gray-700">
+            <MapPin className="mr-1 h-4 w-4" />
+            강남구
+          </button>
+          <button className="flex items-center text-gray-700">
+            <Bell className="mr-1 h-4 w-4" />
+          </button>
+          <button className="text-sm text-gray-700">
+            <Menu className="mr-1 h-4 w-4" />
+          </button>
+        </div>
       </div>
+      <SearchBar />
     </nav>
   );
 };
--- frontend/apps/customer/src/pages/Home.tsx
+++ frontend/apps/customer/src/pages/Home.tsx
@@ -1,17 +1,13 @@
 import FilterBar from '@/components/pages/home/FilterBar';
 import HomeMainContainer from '@/components/pages/home/HomeMainContainer';
-import SearchBar from '@/components/pages/home/SearchBar';
 import { dummyStoreData } from '@/constants/homeMockData';
 
 const Home = () => {
   const stores = dummyStoreData.body.content;
 
   return (
-    <div className="">
-      <div className="sticky">
-        <SearchBar />
-      </div>
-      <div className="sticky">
+    <div>
+      <div className="sticky pt-5">
         <FilterBar />
       </div>
       <HomeMainContainer stores={stores}></HomeMainContainer>
