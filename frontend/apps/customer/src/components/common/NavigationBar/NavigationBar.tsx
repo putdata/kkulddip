0658899ee6e08f2574383a3117869a985a9cdf79
young.yj.cho <young.yj.cho@gmail.com>
2025-08-13 18:34:18 +0900

    fix: 내브바에 서치바 추가 머지 충돌 수정

--- frontend/apps/customer/src/components/common/NavigationBar/NavigationBar.tsx
+++ frontend/apps/customer/src/components/common/NavigationBar/NavigationBar.tsx
@@ -1,3 +1,4 @@
+import SearchBar from '@/components/pages/home/SearchBar';
 import { ROUTE_PATH } from '@/router';
 import { MapPin, Bell, ShoppingCart } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
