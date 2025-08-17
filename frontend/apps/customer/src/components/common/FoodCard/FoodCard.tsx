60a0e3706e0a54944da7181a2a3384e631300a8e
young-droid <young.yj.cho@gmail.com>
2025-08-17 10:47:48 +0900

    chore: format 체크

--- frontend/apps/customer/src/components/common/FoodCard/FoodCard.tsx
+++ frontend/apps/customer/src/components/common/FoodCard/FoodCard.tsx
@@ -80,14 +80,15 @@ const FoodCard = ({ store, onClick }: FoodCardProps) => {
             </span>
 
             {/* 거리 */}
-            {store.distanceFromUser !== undefined && store.distanceFromUser !== null && (
-              <>
-                <span className="text-gray-400">📍</span>
-                <span className="text-sm text-gray-600">
-                  {store.distanceFromUser.toFixed(2)}km
-                </span>
-              </>
-            )}
+            {store.distanceFromUser !== undefined &&
+              store.distanceFromUser !== null && (
+                <>
+                  <span className="text-gray-400">📍</span>
+                  <span className="text-sm text-gray-600">
+                    {store.distanceFromUser.toFixed(2)}km
+                  </span>
+                </>
+              )}
           </div>
 
           {/* TODO: 남은 재고 수 합의 필요 */}
